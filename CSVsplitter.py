ff40dab6e2fe4919f0fc9d7fa712acff673901d2
markosluga <35494371+markosluga@users.noreply.github.com>
2025-05-10 10:01:39 -0400

    Update CSVsplitter.py
    updated the CSVsample name to match the CSV samples  in this repo

--- CSVsplitter.py
+++ CSVsplitter.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 # Load the CSV file
-df = pd.read_csv('contest.csv', encoding='cp1252')
+# In the line below, change the CSVsample name to the name of your csv file
+df = pd.read_csv('CSVsample.csv', encoding='cp1252')
 
 # Define the number of records per split
 chunk_size = 25
@@ -9,4 +10,5 @@ chunk_size = 25
 # Split the DataFrame into chunks
 for i in range(0, len(df), chunk_size):
     chunk = df.iloc[i:i + chunk_size]
-    chunk.to_csv(f'contest{i // chunk_size + 1}.csv', index=False)
+    # In the line below, change the CSVsample name to the name of your csv file
+    chunk.to_csv(f'CSVsample{i // chunk_size + 1}.csv', index=False)
